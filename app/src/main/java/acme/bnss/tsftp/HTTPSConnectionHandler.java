064e823d9c1f8b1b45594289408ed2888f7cc776
eborgs <eborgs@kth.se>
2016-03-04 17:49:27 +0100

    Added a class.

---
+++ app/src/main/java/acme/bnss/tsftp/HTTPSConnectionHandler.java
@@ -0,0 +1,15 @@
+package acme.bnss.tsftp;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Erik Borgstrom on 2016-03-04.
+ */
+public class HTTPSConnectionHandler {
+
+    public HttpsURLConnection getConnectionToACMEWebServer() {
+        
+        return null;
+    }
+
+}
