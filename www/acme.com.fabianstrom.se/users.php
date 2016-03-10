ce56948feab13490572db62cd3344c128223b1af
root <root@bnss-g01-vm3.defaultdomain>
2016-03-10 16:10:43 +0100

    lade till www-katalog

---
+++ www/acme.com.fabianstrom.se/users.php
@@ -0,0 +1,17 @@
+<?php
+
+$users = array(
+'foo.bar@acme.com' => array(
+        'password' => 'aecf23c76ee9654a49965bd076754f812bc69349000b7e089c5adfc6c23a31eb', // foobar
+        'hmac_key' => '016a8d6694e5b6ae100c0a99e84d9dd66a95a76372efa2da0ff75343750ade00',
+        'totp_key' => 'SKZXGEGY6FDZSHK3'
+),
+'cat.catz@acme.com' => array(
+	'password' => '1e34a495a7c43efa0d7927808930fdc7ac925ef9976330b3330e53e43e720ae4', // catz
+	'hmac_key' => '3cuhs9BDbGMxlN/RFsagl+KAY78fueE1WRHH',
+	'totp_key' => '2EJFVOBWSJ7MR2L3',
+),
+);
+
+
+?>
